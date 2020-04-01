5289f1daca3d21b3db5621f324a3070c9687c312
c00kamunga <tsothalanti2@yahoo.com>
2020-04-01 16:43:45 -0700

    added h1 as to do list, added a form, added UL and LI hardcoded

---
+++ to-do-list/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+<h1>
+  To Do List
+</h1>
+
+<form>
+  <input placeholder="your to-do..."/>
+  <ul>
+    <li>Buy Milk</li>
+    <li>Buy Cheese</li>
+  </ul>
+</form>
+    </div>
+  );
+}
+
+export default App;
