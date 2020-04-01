c5b962e6e68c5227b170e7a494fe75ec4303bd08
c00kamunga <tsothalanti2@yahoo.com>
2020-04-01 16:46:41 -0700

    imported useState from react, set up state with initial value as an empty string

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 
 function App() {
+  const [newToDo, setNewToDo] = useState('')
   return (
     <div className="App">
 <h1>
